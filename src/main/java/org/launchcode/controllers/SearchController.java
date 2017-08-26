1930be7bc43b0e30f63e41dbcc336435f3b109dd
William Toscano <william_toscano_@hotmail.com>
2017-08-26 01:56:48 -0700

    Added "no jobs found" for when the search function can't find any entries

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -33,15 +33,24 @@ public class SearchController {
 
 
             ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm); //Creates the array List of hashmaps that will be displayed (before formating)
-
-            model.addAttribute("title", "Jobs with " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
-            model.addAttribute("jobs", jobs);
+            if (jobs.isEmpty()){
+                model.addAttribute("title", "No jobs found for " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
+            }
+            else {
+                model.addAttribute("title", "Jobs from " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
+                model.addAttribute("jobs", jobs);
+            }
         }
         else {
 
             ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType,searchTerm);
-            model.addAttribute("title", "Jobs with " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
-            model.addAttribute("jobs", jobs);
+            if (jobs.isEmpty()){
+                model.addAttribute("title", "No jobs found for " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
+            }
+            else {
+                model.addAttribute("title", "Jobs with " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
+                model.addAttribute("jobs", jobs);
+            }
         }
             model.addAttribute("columns", ListController.columnChoices);
 
